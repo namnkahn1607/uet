a3412a7c23e80d72b6a12e177a54905186b6074f
namnkahn1607 <nlnkandthebois@gmail.com>
2025-05-03 23:58:49 +0700

    12.cpp

--- advanced programme/11-20/12.cpp
+++ advanced programme/11-20/12.cpp
@@ -1,7 +1,32 @@
 // adv-programme.12
 #include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <numeric>
 using namespace std;
 
 int main() {
-    // add
+    double val;
+    vector<double> arr;
+
+    while (cin >> val) {
+        arr.emplace_back(val);
+        
+        if (cin.peek() == '\n')
+            break;
+    }
+
+    double mean = accumulate(begin(arr), end(arr), 0.0) / arr.size();
+
+    double variance = 0.0;
+
+    for (double &d : arr)
+        variance += pow(d - mean, 2);
+
+    variance /= arr.size();
+
+    cout << fixed << setprecision(2) << mean << " " << variance << '\n';
+
+    return 0;
 }
