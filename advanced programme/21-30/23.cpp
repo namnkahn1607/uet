63fa9570358611274335281163f8d10f15063d9b
namnkahn1607 <nlnkandthebois@gmail.com>
2025-05-05 15:47:40 +0700

    23.cpp

--- advanced programme/21-30/23.cpp
+++ advanced programme/21-30/23.cpp
@@ -4,4 +4,42 @@ using namespace std;
 
 struct Node {
     const int value;
+    Node* next;
+
+    Node(int x) : value(x), next(nullptr) {}
+};
+
+class LinkedList {
+    Node* head;
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    LinkedList(int arr[], int n) {
+        head = new Node(arr[0]);
+        Node* tail = head;
+
+        for (int i = 1; i < n; ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+    }
+
+    void insertHead(int x) {
+        Node* newHead = new Node(x);
+        newHead->next = head;
+        head = newHead;
+    }
+
+    void print() {
+        Node* cur = head;
+
+        while (cur) {
+            cout << cur->value << ' ';
+            cur = cur->next;
+        }
+    }
 }
+
+int main() {}
