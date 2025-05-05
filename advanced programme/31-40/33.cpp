0c9dc67299e5919c3a67b7f4603371e91f8a8bb1
namnkahn1607 <nlnkandthebois@gmail.com>
2025-05-05 23:34:10 +0700

    33.cpp

---
+++ advanced programme/31-40/33.cpp
@@ -0,0 +1,33 @@
+// adv-programme.33
+#include <iostream>
+using namespace std;
+
+int main() {
+    int l, r; cin >> l >> r;
+
+    auto distinct = [](int x) {
+        int arr[10] = {};
+
+        while (x) {
+            if (arr[x % 10] > 0)
+                return false;
+
+            ++arr[x % 10];
+            x /= 10;
+        }
+
+        return true;
+    };
+
+    for (int i = l; i <= r; ++i) {
+        if (distinct(i)) {
+            cout << i;
+            
+            return 0;
+        }
+    }
+
+    cout << -1;
+
+    return 0;
+}
