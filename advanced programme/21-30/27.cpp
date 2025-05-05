73ff09774e67a27bf0ddb8342bed7019e9590fbe
namnkahn1607 <nlnkandthebois@gmail.com>
2025-05-05 16:15:58 +0700

    27.cpp

---
+++ advanced programme/21-30/27.cpp
@@ -0,0 +1,67 @@
+// adv-programme.27
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int value;
+    Node* next;
+};
+
+Node* createNode(const int &x) {
+    Node* newNode = new Node;
+    newNode->value = x;
+    newNode->next = nullptr;
+
+    return newNode;
+}
+
+Node* createNode(const int &x, Node* ptr) {
+    Node* newNode = new Node;
+    newNode->value = x;
+    newNode->next = ptr;
+
+    return newNode;
+}
+
+Node* createLList(const int &m) {
+    if (m == 0) return nullptr;
+
+    int x; cin >> x;
+    Node* head = createNode(x);
+    Node* tail = head;
+
+    for (int i = 1; i < m; ++i) {
+        cin >> x;
+        tail->next = createNode(x);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+void printLList(Node* head) {
+    while (head) {
+        cout << head->value << ' ';
+        head = head->next;
+    }
+
+    cout << '\n';
+}
+
+Node* insertHead(Node* head, int x) {
+    Node* newHead = createNode(x);
+    newHead->next = head;
+
+    return newHead;
+}
+
+int main() {
+    int m, x; cin >> m >> x;
+    Node* head = createLList(m);
+
+    head = insertHead(head, x);
+
+    printLList(head);
+
+    return 0;
+}
